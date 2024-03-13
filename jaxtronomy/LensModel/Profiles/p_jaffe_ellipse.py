1bf8d363177548f2719338cadaa0c84dc0c6923f
Aymeric Galan <aymeric.galan@gmail.com>
2024-03-13 09:21:44 +0100

    Add elliptical pseudo-Jaffe, param_util and related tests

---
+++ jaxtronomy/LensModel/Profiles/p_jaffe_ellipse.py
@@ -0,0 +1,127 @@
+from jaxtronomy.LensModel.Profiles.p_jaffe import PJaffe
+import jaxtronomy.Util.param_util as param_util
+from lenstronomy.LensModel.Profiles.base_profile import LensProfileBase
+import jax.numpy as jnp
+import numpy as np
+
+__all__ = ["PJaffe_Ellipse"]
+
+
+class PJaffe_Ellipse(LensProfileBase):
+    """
+    class to compute the DUAL PSEUDO ISOTHERMAL ELLIPTICAL MASS DISTRIBUTION
+    based on Eliasdottir (2007) https://arxiv.org/pdf/0710.5636.pdf Appendix A
+    with the ellipticity implemented in the potential
+
+    Module name: 'PJAFFE_ELLIPSE';
+
+    An alternative name is dPIED.
+
+    The 3D density distribution is
+
+    .. math::
+        \\rho(r) = \\frac{\\rho_0}{(1+r^2/Ra^2)(1+r^2/Rs^2)}
+
+    with :math:`Rs > Ra`.
+
+    The projected density is
+
+    .. math::
+        \\Sigma(R) = \\Sigma_0 \\frac{Ra Rs}{Rs-Ra}\\left(\\frac{1}{\\sqrt{Ra^2+R^2}} - \\frac{1}{\\sqrt{Rs^2+R^2}} \\right)
+
+    with
+
+    .. math::
+        \\Sigma_0 = \\pi \\rho_0 \\frac{Ra Rs}{Rs + Ra}
+
+    In the lensing parameterization,
+
+    .. math::
+        \\sigma_0 = \\frac{\\Sigma_0}{\\Sigma_{\\rm crit}}
+
+    """
+
+    param_names = ["sigma0", "Ra", "Rs", "e1", "e2", "center_x", "center_y"]
+    lower_limit_default = {
+        "sigma0": 0,
+        "Ra": 0,
+        "Rs": 0,
+        "e1": -0.5,
+        "e2": -0.5,
+        "center_x": -100,
+        "center_y": -100,
+    }
+    upper_limit_default = {
+        "sigma0": 10,
+        "Ra": 100,
+        "Rs": 100,
+        "e1": 0.5,
+        "e2": 0.5,
+        "center_x": 100,
+        "center_y": 100,
+    }
+
+    def __init__(self):
+        self.spherical = PJaffe()
+        self._diff = 0.000001
+        super(PJaffe_Ellipse, self).__init__()
+
+    def function(self, x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0):
+        """Returns double integral of NFW profile."""
+        x_, y_ = param_util.transform_e1e2_square_average(
+            x, y, e1, e2, center_x, center_y
+        )
+        f_ = self.spherical.function(x_, y_, sigma0, Ra, Rs)
+        return f_
+
+    def derivatives(self, x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0):
+        """Returns df/dx and df/dy of the function (integral of NFW)"""
+        phi_G, q = param_util.ellipticity2phi_q(e1, e2)
+        x_, y_ = param_util.transform_e1e2_square_average(
+            x, y, e1, e2, center_x, center_y
+        )
+        e = param_util.q2e(q)
+        cos_phi = jnp.cos(phi_G)
+        sin_phi = jnp.sin(phi_G)
+        f_x_prim, f_y_prim = self.spherical.derivatives(
+            x_, y_, sigma0, Ra, Rs, center_x=0, center_y=0
+        )
+        f_x_prim *= jnp.sqrt(1 - e)
+        f_y_prim *= jnp.sqrt(1 + e)
+        f_x = cos_phi * f_x_prim - sin_phi * f_y_prim
+        f_y = sin_phi * f_x_prim + cos_phi * f_y_prim
+        return f_x, f_y
+
+    def hessian(self, x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0):
+        """Returns Hessian matrix of function d^2f/dx^2, d^2/dxdy, d^2/dydx,
+        d^f/dy^2."""
+        alpha_ra, alpha_dec = self.derivatives(
+            x, y, sigma0, Ra, Rs, e1, e2, center_x, center_y
+        )
+        diff = self._diff
+        alpha_ra_dx, alpha_dec_dx = self.derivatives(
+            x + diff, y, sigma0, Ra, Rs, e1, e2, center_x, center_y
+        )
+        alpha_ra_dy, alpha_dec_dy = self.derivatives(
+            x, y + diff, sigma0, Ra, Rs, e1, e2, center_x, center_y
+        )
+
+        f_xx = (alpha_ra_dx - alpha_ra) / diff
+        f_xy = (alpha_ra_dy - alpha_ra) / diff
+        f_yx = (alpha_dec_dx - alpha_dec) / diff
+        f_yy = (alpha_dec_dy - alpha_dec) / diff
+
+        return f_xx, f_xy, f_yx, f_yy
+
+    def mass_3d_lens(self, r, sigma0, Ra, Rs, e1=0, e2=0):
+        """
+
+        :param r:
+        :param sigma0:
+        :param Ra:
+        :param Rs:
+        :param e1:
+        :param e2:
+        :return:
+        """
+        return self.spherical.mass_3d_lens(r, sigma0, Ra, Rs)
---
+++ test/test_LensModel/test_Profiles/test_p_jaffe_ellipse.py
@@ -0,0 +1,107 @@
+__author__ = "sibirrer"
+
+
+from jaxtronomy.LensModel.Profiles.p_jaffe_ellipse import PJaffe_Ellipse
+import jaxtronomy.Util.param_util as param_util
+
+import numpy as np
+import numpy.testing as npt
+import pytest
+import jax
+jax.config.update('jax_enable_x64', True)  # 64-bit floats, consistent with numpy
+import jax.numpy as jnp
+
+
+class TestP_JAFFW(object):
+    """Tests the Gaussian methods."""
+
+    def setup_method(self):
+        self.profile = PJaffe_Ellipse()
+
+    def test_function(self):
+        x = jnp.array([1])
+        y = jnp.array([2])
+        sigma0 = 1.0
+        Ra, Rs = 0.5, 0.8
+        q, phi_G = 0.8, 0
+        e1, e2 = param_util.phi_q2_ellipticity(phi_G, q)
+        values = self.profile.function(
+            x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0
+        )
+        npt.assert_almost_equal(values[0], 0.9091040398607811, decimal=8)
+        x = jnp.array([0])
+        y = jnp.array([0])
+
+        values = self.profile.function(
+            x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0
+        )
+        npt.assert_almost_equal(values[0], 0.20267440905756931, decimal=8)
+
+        x = jnp.array([2, 3, 4])
+        y = jnp.array([1, 1, 1])
+        values = self.profile.function(
+            x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0
+        )
+        npt.assert_almost_equal(values[0], 0.8327830942970774, decimal=8)
+        npt.assert_almost_equal(values[1], 1.0233085474140422, decimal=8)
+        npt.assert_almost_equal(values[2], 1.1868752663038047, decimal=8)
+
+    def test_derivatives(self):
+        x = jnp.array([1])
+        y = jnp.array([2])
+        sigma0 = 1.0
+        Ra, Rs = 0.5, 0.8
+        q, phi_G = 0.8, 0
+        e1, e2 = param_util.phi_q2_ellipticity(phi_G, q)
+        f_x, f_y = self.profile.derivatives(
+            x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0
+        )
+        npt.assert_almost_equal(f_x[0], 0.08130928181117723, decimal=8)
+        npt.assert_almost_equal(f_y[0], 0.25409150565992883, decimal=8)
+        x = jnp.array([0])
+        y = jnp.array([0])
+        f_x, f_y = self.profile.derivatives(
+            x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0
+        )
+        assert f_x[0] == 0
+        assert f_y[0] == 0
+
+        x = jnp.array([1, 3, 4])
+        y = jnp.array([2, 1, 1])
+        values = self.profile.derivatives(
+            x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0
+        )
+        npt.assert_almost_equal(values[0][0], 0.08130928181117723, decimal=8)
+        npt.assert_almost_equal(values[1][0], 0.25409150565992883, decimal=8)
+        npt.assert_almost_equal(values[0][1], 0.17711143165920576, decimal=8)
+        npt.assert_almost_equal(values[1][1], 0.09224553732250299, decimal=8)
+
+    def test_hessian(self):
+        x = jnp.array([1])
+        y = jnp.array([2])
+        sigma0 = 1.0
+        Ra, Rs = 0.5, 0.8
+        q, phi_G = 0.8, 0
+        e1, e2 = param_util.phi_q2_ellipticity(phi_G, q)
+        f_xx, f_xy, f_yx, f_yy = self.profile.hessian(
+            x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0
+        )
+        npt.assert_almost_equal(f_xx[0], 0.06259391932550429, decimal=8)
+        npt.assert_almost_equal(f_yy[0], -0.05572123112917993, decimal=8)
+        npt.assert_almost_equal(f_xy[0], -0.058485405643460275, decimal=8)
+        npt.assert_almost_equal(f_xy, f_yx, decimal=6)
+        x = jnp.array([1, 3, 4])
+        y = jnp.array([2, 1, 1])
+        values = self.profile.hessian(
+            x, y, sigma0, Ra, Rs, e1, e2, center_x=0, center_y=0
+        )
+        npt.assert_almost_equal(values[0][0], 0.06259391932550429, decimal=8)
+        npt.assert_almost_equal(values[3][0], -0.05572123112917993, decimal=8)
+
+    def test_mass_3d_lens(self):
+        mass = self.profile.mass_3d_lens(r=1, sigma0=1, Ra=0.5, Rs=0.8, e1=0, e2=0)
+        npt.assert_almost_equal(mass, 0.87077306005349242, decimal=8)
+
+
+if __name__ == "__main__":
+    pytest.main()
