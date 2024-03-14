a614f87c7582ef4c7761a99041bd74e3c0bbde93
Anowar Shajib <ajshajib@uchicago.edu>
2024-03-14 10:53:53 +0000

    Set up autoformatting

--- jaxtronomy/__init__.py
+++ jaxtronomy/__init__.py
@@ -1,2 +1,2 @@
-__author__ = 'lenstronomy developers'
-__version__ = '0.0.1rc1'
+__author__ = "lenstronomy developers"
+__version__ = "0.0.1rc1"
--- test/test_lenstronomy_import.py
+++ test/test_lenstronomy_import.py
@@ -1,7 +1,7 @@
-
 def test_lenstronomy_version():
     """
     tests the import of lenstronomy
     """
     import lenstronomy
-    assert lenstronomy.__version__ == '1.10.4'
+
+    assert lenstronomy.__version__ == "1.10.4"
