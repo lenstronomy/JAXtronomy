8d102eb298ef68d72f8c3323e5c93dde5a8ad4a6
nataliehogg <mail@nataliebhogg.com>
2024-06-13 18:15:34 +0200

    update singleplane tests

--- test/test_LensModel/test_single_plane.py
+++ test/test_LensModel/test_single_plane.py
@@ -4,7 +4,10 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 from jaxtronomy.LensModel.single_plane import SinglePlane
+from lenstronomy.LensModel.single_plane import SinglePlane as SinglePlane_ref
 from jaxtronomy.LensModel.Profiles.sis import SIS
+from lenstronomy.LensModel.Profiles.sis import SIS_ref
+
 import unittest
 
 import jax
@@ -25,6 +28,8 @@ class TestLensModel(object):
 
     def setup_method(self):
         self.lensModel = SinglePlane(["GAUSSIAN"])
+        self.lensModel_ref = SinglePlane_ref(["GAUSSIAN"])
+
         self.kwargs = [
             {
                 "amp": 1.0,
@@ -37,35 +42,66 @@ class TestLensModel(object):
 
     def test_potential(self):
         output = self.lensModel.potential(x=1.0, y=1.0, kwargs=self.kwargs)
+        output_ref = self.lensModel_ref.potential(x=1.0, y=1.0, kwargs=self.kwargs)
         assert output == 0.77880078307140488 / (8 * jnp.pi)
+        assert output == output_ref
 
     def test_alpha(self):
         output1, output2 = self.lensModel.alpha(x=1.0, y=1.0, kwargs=self.kwargs)
+        output1_ref, output2_ref = self.lensModel_ref.alpha(x=1.0, y=1.0, kwargs=self.kwargs)
         assert output1 == -0.19470019576785122 / (8 * jnp.pi)
         assert output2 == -0.19470019576785122 / (8 * jnp.pi)
+        assert output1 == output1_ref
+
+        assert output1 == output1_ref
+        assert output2 == output2_ref
+
 
     def test_ray_shooting(self):
         delta_x, delta_y = self.lensModel.ray_shooting(x=1.0, y=1.0, kwargs=self.kwargs)
+        delta_x_ref, delta_y_ref = self.lensModel_ref.ray_shooting(x=1.0, y=1.0, kwargs=self.kwargs)
+
         assert delta_x == 1 + 0.19470019576785122 / (8 * jnp.pi)
         assert delta_y == 1 + 0.19470019576785122 / (8 * jnp.pi)
 
+        assert delta_x = delta_x_ref
+        assert delta_y = delta_y_ref
+
+
     def test_mass_2d(self):
         lensModel = SinglePlane(["GAUSSIAN_KAPPA"])
+        lensModel_ref = SinglePlane_ref(["GAUSSIAN_KAPPA"])
         kwargs = [{"amp": 1.0, "sigma": 2.0, "center_x": 0.0, "center_y": 0.0}]
         output = lensModel.mass_2d(r=1, kwargs=kwargs)
+        output_ref = lensModel_ref.mass_2d(r=1, kwargs=kwargs)
         npt.assert_almost_equal(output, 0.11750309741540453, decimal=9)
+        npt.assert_almost_equal(output, output_ref, decimal=9)
+
 
     def test_density(self):
         theta_E = 1
         r = 1
         lensModel = SinglePlane(lens_model_list=["SIS"])
+        lensModel_ref = SinglePlane_ref(lens_model_list=["SIS"])
+
         density = lensModel.density(r=r, kwargs=[{"theta_E": theta_E}])
+        density_ref = lensModel_ref.density(r=r, kwargs=[{"theta_E": theta_E}])
+
         sis = SIS()
+        sis_ref = SIS_ref()
+
         density_model = sis.density_lens(r=r, theta_E=theta_E)
+        density_model_ref = sis_ref.density_lens(r=r, theta_E=theta_E)
+
         npt.assert_almost_equal(density, density_model, decimal=8)
+        npt.assert_almost_equal(density, density_ref, decimal=8)
+        npt.assert_almost_equal(density_model, density_model_ref, decimal=8)
+
 
     def test_bool_list(self):
         lensModel = SinglePlane(["SPEP", "SHEAR"])
+        lensModel_ref = SinglePlane_ref(["SPEP", "SHEAR"])
+
         kwargs = [
             {
                 "theta_E": 1,
@@ -77,16 +113,41 @@ class TestLensModel(object):
             },
             {"gamma1": 0.01, "gamma2": -0.02},
         ]
+
         alphax_1, alphay_1 = lensModel.alpha(1, 1, kwargs, k=0)
+        alphax_1_ref, alphay_1_ref = lensModel_ref.alpha(1, 1, kwargs, k=0)
+
         alphax_1_list, alphay_1_list = lensModel.alpha(1, 1, kwargs, k=[0])
+        alphax_1_list_ref, alphay_1_list_ref = lensModel_ref.alpha(1, 1, kwargs, k=[0])
+
         npt.assert_almost_equal(alphax_1, alphax_1_list, decimal=5)
         npt.assert_almost_equal(alphay_1, alphay_1_list, decimal=5)
+
+        npt.assert_almost_equal(alphax_1, alphax_1_ref)
+        npt.assert_almost_equal(alphay_1, alphay_1_ref)
+
+        npt.assert_almost_equal(alphax_1_list, alphax_1_list_ref)
+        npt.assert_almost_equal(alphay_1_list, alphay_1_list_ref)
+
 
         alphax_1_1, alphay_1_1 = lensModel.alpha(1, 1, kwargs, k=0)
         alphax_1_2, alphay_1_2 = lensModel.alpha(1, 1, kwargs, k=1)
         alphax_full, alphay_full = lensModel.alpha(1, 1, kwargs, k=None)
-        npt.assert_almost_equal(alphax_1_1 + alphax_1_2, alphax_full, decimal=5)
-        npt.assert_almost_equal(alphay_1_1 + alphay_1_2, alphay_full, decimal=5)
+
+        alphax_1_1_ref, alphay_1_1_ref = lensModel_ref.alpha(1, 1, kwargs, k=0)
+        alphax_1_2_ref, alphay_1_2_ref = lensModel_ref_ref.alpha(1, 1, kwargs, k=1)
+        alphax_full_ref, alphay_full_ref = lensModel_ref.alpha(1, 1, kwargs, k=None)
+
+        npt.assert_almost_equal(alphax_1_1_ref + alphax_1_2_ref, alphax_full_ref, decimal=5)
+        npt.assert_almost_equal(alphay_1_1_ref + alphay_1_2_ref, alphay_full_ref, decimal=5)
+
+        npt.assert_almost_equal(alphax_1_1, alphax_1_1_ref)
+        npt.assert_almost_equal(alphax_1_2, alphax_1_2_ref)
+        npt.assert_almost_equal(alphay_1_1, alphay_1_1_ref)
+        npt.assert_almost_equal(alphay_1_2, alphay_1_2_ref)
+        npt.assert_almost_equal(alphax_full, alphax_full_ref)
+        npt.assert_almost_equal(alphay_full, alphay_full_ref)
+
 
     def test_init(self):
         lens_model_list = [
@@ -102,6 +163,7 @@ class TestLensModel(object):
         ]
         lensModel = SinglePlane(lens_model_list=lens_model_list)
         assert lensModel.func_list[0].param_names[0] == "Rs"
+
 
 
 class TestRaise(unittest.TestCase):
