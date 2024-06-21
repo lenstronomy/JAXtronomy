e2b66b00a5cbed77e174f7cbe23c49c8e61a5312
nataliehogg <mail@nataliebhogg.com>
2024-06-21 15:03:40 +0200

    removed test against values

--- test/test_LensModel/test_Profiles/test_sis.py
+++ test/test_LensModel/test_Profiles/test_sis.py
@@ -1,12 +1,15 @@
 __author__ = "sibirrer"
 
-
 from jaxtronomy.LensModel.Profiles.sis import SIS
 from lenstronomy.LensModel.Profiles.sis import SIS as SIS_ref
 
 import numpy as np
 import numpy.testing as npt
 import pytest
+import jax
+
+jax.config.update("jax_enable_x64", True)
+import jax.numpy as jnp
 
 
 class TestSIS(object):
@@ -45,8 +48,6 @@ class TestSIS(object):
         phi_E = 1.0
         f_x, f_y = self.SIS.derivatives(x, y, phi_E)
         f_x_ref, f_y_ref = self.SIS_ref.derivatives(x, y, phi_E)
-        npt.assert_almost_equal(f_x[0], 0.44721359549995793, decimal=9)
-        npt.assert_almost_equal(f_y[0], 0.89442719099991586, decimal=9)
         npt.assert_almost_equal(f_x, f_x_ref, decimal=9)
         npt.assert_almost_equal(f_y, f_y_ref, decimal=9)
         x = np.array([0])
@@ -61,9 +62,6 @@ class TestSIS(object):
         phi_E = 1.0
         f_xx, f_xy, f_yx, f_yy = self.SIS.hessian(x, y, phi_E)
         f_xx_ref, f_xy_ref, f_yx_ref, f_yy_ref = self.SIS_ref.hessian(x, y, phi_E)
-        npt.assert_almost_equal(f_xx[0], 0.35777087639996635, decimal=9)
-        npt.assert_almost_equal(f_yy[0], 0.089442719099991588, decimal=9)
-        npt.assert_almost_equal(f_xy[0], -0.17888543819998318, decimal=9)
         npt.assert_almost_equal(f_xy, f_yx, decimal=8)
         npt.assert_almost_equal(f_xx, f_xx_ref, decimal=9)
         npt.assert_almost_equal(f_xy, f_xy_ref, decimal=9)
@@ -73,12 +71,6 @@ class TestSIS(object):
         y = np.array([2, 1, 1])
         values = self.SIS.hessian(x, y, phi_E)
         values_ref = self.SIS_ref.hessian(x, y, phi_E)
-        npt.assert_almost_equal(values[0][0], 0.35777087639996635, decimal=9)
-        npt.assert_almost_equal(values[3][0], 0.089442719099991588, decimal=9)
-        npt.assert_almost_equal(values[1][0], -0.17888543819998318, decimal=9)
-        npt.assert_almost_equal(values[0][1], 0.031622776601683791, decimal=9)
-        npt.assert_almost_equal(values[3][1], 0.28460498941515411, decimal=9)
-        npt.assert_almost_equal(values[1][1], -0.094868329805051374, decimal=9)
         npt.assert_almost_equal(values, values_ref)
 
     def test_theta2rho(self):
