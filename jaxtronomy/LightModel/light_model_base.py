2c095afd6cbca6092d624ddf1f0d1d81d30db664
ahuang314 <alan.huang.1@stonybrook.edu>
2024-09-02 01:42:01 -0400

    fixed comment formatting

--- jaxtronomy/LightModel/light_model_base.py
+++ jaxtronomy/LightModel/light_model_base.py
@@ -69,30 +69,28 @@ class LightModelBase(object):
         if sersic_major_axis is None:
             sersic_major_axis = sersic_major_axis_conf
         for profile_type in light_model_list:
-            """
-            if profile_type == "GAUSSIAN":
-                from lenstronomy.LightModel.Profiles.gaussian import Gaussian
+            # if profile_type == "GAUSSIAN":
+            #     from lenstronomy.LightModel.Profiles.gaussian import Gaussian
 
-                self.func_list.append(Gaussian())
-            elif profile_type == "GAUSSIAN_ELLIPSE":
-                from lenstronomy.LightModel.Profiles.gaussian import GaussianEllipse
+            #     self.func_list.append(Gaussian())
+            # elif profile_type == "GAUSSIAN_ELLIPSE":
+            #     from lenstronomy.LightModel.Profiles.gaussian import GaussianEllipse
 
-                self.func_list.append(GaussianEllipse())
-            elif profile_type == "ELLIPSOID":
-                from lenstronomy.LightModel.Profiles.ellipsoid import Ellipsoid
+            #     self.func_list.append(GaussianEllipse())
+            # elif profile_type == "ELLIPSOID":
+            #     from lenstronomy.LightModel.Profiles.ellipsoid import Ellipsoid
 
-                self.func_list.append(Ellipsoid())
-            elif profile_type == "MULTI_GAUSSIAN":
-                from lenstronomy.LightModel.Profiles.gaussian import MultiGaussian
+            #     self.func_list.append(Ellipsoid())
+            # elif profile_type == "MULTI_GAUSSIAN":
+            #     from lenstronomy.LightModel.Profiles.gaussian import MultiGaussian
 
-                self.func_list.append(MultiGaussian())
-            elif profile_type == "MULTI_GAUSSIAN_ELLIPSE":
-                from lenstronomy.LightModel.Profiles.gaussian import (
-                    MultiGaussianEllipse,
-                )
+            #     self.func_list.append(MultiGaussian())
+            # elif profile_type == "MULTI_GAUSSIAN_ELLIPSE":
+            #     from lenstronomy.LightModel.Profiles.gaussian import (
+            #         MultiGaussianEllipse,
+            #     )
 
-                self.func_list.append(MultiGaussianEllipse())
-            """
+            #     self.func_list.append(MultiGaussianEllipse())
             if profile_type == "SERSIC":
                 from jaxtronomy.LightModel.Profiles.sersic import Sersic
 
@@ -120,96 +118,96 @@ class LightModelBase(object):
                     CoreSersic(smoothing=smoothing, sersic_major_axis=sersic_major_axis)
                 )
             
-            #elif profile_type == "SHAPELETS":
-            #    from lenstronomy.LightModel.Profiles.shapelets import ShapeletSet
+            # elif profile_type == "SHAPELETS":
+            #     from lenstronomy.LightModel.Profiles.shapelets import ShapeletSet
 
-            #    self.func_list.append(ShapeletSet())
-            #elif profile_type == "SHAPELETS_ELLIPSE":
-            #    from lenstronomy.LightModel.Profiles.shapelets_ellipse import (
-            #        ShapeletSetEllipse,
-            #    )
+            #     self.func_list.append(ShapeletSet())
+            # elif profile_type == "SHAPELETS_ELLIPSE":
+            #     from lenstronomy.LightModel.Profiles.shapelets_ellipse import (
+            #         ShapeletSetEllipse,
+            #     )
 
-            #    self.func_list.append(ShapeletSetEllipse())
-            #elif profile_type == "SHAPELETS_POLAR":
-            #    from lenstronomy.LightModel.Profiles.shapelets_polar import (
-            #        ShapeletSetPolar,
-            #    )
+            #     self.func_list.append(ShapeletSetEllipse())
+            # elif profile_type == "SHAPELETS_POLAR":
+            #     from lenstronomy.LightModel.Profiles.shapelets_polar import (
+            #         ShapeletSetPolar,
+            #     )
 
-            #    self.func_list.append(ShapeletSetPolar(exponential=False))
-            #elif profile_type == "SHAPELETS_POLAR_EXP":
-            #    from lenstronomy.LightModel.Profiles.shapelets_polar import (
-            #        ShapeletSetPolar,
-            #    )
+            #     self.func_list.append(ShapeletSetPolar(exponential=False))
+            # elif profile_type == "SHAPELETS_POLAR_EXP":
+            #     from lenstronomy.LightModel.Profiles.shapelets_polar import (
+            #         ShapeletSetPolar,
+            #     )
 
-            #    self.func_list.append(ShapeletSetPolar(exponential=True))
-            #elif profile_type == "HERNQUIST":
-            #    from lenstronomy.LightModel.Profiles.hernquist import Hernquist
+            #     self.func_list.append(ShapeletSetPolar(exponential=True))
+            # elif profile_type == "HERNQUIST":
+            #     from lenstronomy.LightModel.Profiles.hernquist import Hernquist
 
-            #    self.func_list.append(Hernquist())
-            #elif profile_type == "HERNQUIST_ELLIPSE":
-            #    from lenstronomy.LightModel.Profiles.hernquist import HernquistEllipse
+            #     self.func_list.append(Hernquist())
+            # elif profile_type == "HERNQUIST_ELLIPSE":
+            #     from lenstronomy.LightModel.Profiles.hernquist import HernquistEllipse
 
-            #    self.func_list.append(HernquistEllipse())
-            #elif profile_type == "PJAFFE":
-            #    from lenstronomy.LightModel.Profiles.pseudo_jaffe import PseudoJaffe
+            #     self.func_list.append(HernquistEllipse())
+            # elif profile_type == "PJAFFE":
+            #     from lenstronomy.LightModel.Profiles.pseudo_jaffe import PseudoJaffe
 
-            #    self.func_list.append(PseudoJaffe())
-            #elif profile_type == "PJAFFE_ELLIPSE":
-            #    from lenstronomy.LightModel.Profiles.pseudo_jaffe import (
-            #        PseudoJaffeEllipse,
-            #    )
+            #     self.func_list.append(PseudoJaffe())
+            # elif profile_type == "PJAFFE_ELLIPSE":
+            #     from lenstronomy.LightModel.Profiles.pseudo_jaffe import (
+            #         PseudoJaffeEllipse,
+            #     )
 
-            #    self.func_list.append(PseudoJaffeEllipse())
-            #elif profile_type == "UNIFORM":
-            #    from lenstronomy.LightModel.Profiles.uniform import Uniform
+            #     self.func_list.append(PseudoJaffeEllipse())
+            # elif profile_type == "UNIFORM":
+            #     from lenstronomy.LightModel.Profiles.uniform import Uniform
 
-            #    self.func_list.append(Uniform())
-            #elif profile_type == "POWER_LAW":
-            #    from lenstronomy.LightModel.Profiles.power_law import PowerLaw
+            #     self.func_list.append(Uniform())
+            # elif profile_type == "POWER_LAW":
+            #     from lenstronomy.LightModel.Profiles.power_law import PowerLaw
 
-            #    self.func_list.append(PowerLaw())
-            #elif profile_type == "NIE":
-            #    from lenstronomy.LightModel.Profiles.nie import NIE
+            #     self.func_list.append(PowerLaw())
+            # elif profile_type == "NIE":
+            #     from lenstronomy.LightModel.Profiles.nie import NIE
 
-            #    self.func_list.append(NIE())
-            #elif profile_type == "CHAMELEON":
-            #    from lenstronomy.LightModel.Profiles.chameleon import Chameleon
+            #     self.func_list.append(NIE())
+            # elif profile_type == "CHAMELEON":
+            #     from lenstronomy.LightModel.Profiles.chameleon import Chameleon
 
-            #    self.func_list.append(Chameleon())
-            #elif profile_type == "DOUBLE_CHAMELEON":
-            #    from lenstronomy.LightModel.Profiles.chameleon import DoubleChameleon
+            #     self.func_list.append(Chameleon())
+            # elif profile_type == "DOUBLE_CHAMELEON":
+            #     from lenstronomy.LightModel.Profiles.chameleon import DoubleChameleon
 
-            #    self.func_list.append(DoubleChameleon())
-            #elif profile_type == "TRIPLE_CHAMELEON":
-            #    from lenstronomy.LightModel.Profiles.chameleon import TripleChameleon
+            #     self.func_list.append(DoubleChameleon())
+            # elif profile_type == "TRIPLE_CHAMELEON":
+            #     from lenstronomy.LightModel.Profiles.chameleon import TripleChameleon
 
-            #    self.func_list.append(TripleChameleon())
-            #elif profile_type == "INTERPOL":
-            #    from lenstronomy.LightModel.Profiles.interpolation import Interpol
+            #     self.func_list.append(TripleChameleon())
+            # elif profile_type == "INTERPOL":
+            #     from lenstronomy.LightModel.Profiles.interpolation import Interpol
 
-            #    self.func_list.append(Interpol())
-            #elif profile_type == "SLIT_STARLETS":
-            #    from lenstronomy.LightModel.Profiles.starlets import SLIT_Starlets
+            #     self.func_list.append(Interpol())
+            # elif profile_type == "SLIT_STARLETS":
+            #     from lenstronomy.LightModel.Profiles.starlets import SLIT_Starlets
 
-            #    self.func_list.append(
-            #        SLIT_Starlets(fast_inverse=True, second_gen=False)
-            #    )
-            #elif profile_type == "SLIT_STARLETS_GEN2":
-            #    from lenstronomy.LightModel.Profiles.starlets import SLIT_Starlets
+            #     self.func_list.append(
+            #         SLIT_Starlets(fast_inverse=True, second_gen=False)
+            #     )
+            # elif profile_type == "SLIT_STARLETS_GEN2":
+            #     from lenstronomy.LightModel.Profiles.starlets import SLIT_Starlets
 
-            #    self.func_list.append(SLIT_Starlets(second_gen=True))
-            #elif profile_type == "LINEAR":
-            #    from lenstronomy.LightModel.Profiles.linear import Linear
+            #     self.func_list.append(SLIT_Starlets(second_gen=True))
+            # elif profile_type == "LINEAR":
+            #     from lenstronomy.LightModel.Profiles.linear import Linear
 
-            #    self.func_list.append(Linear())
-            #elif profile_type == "LINEAR_ELLIPSE":
-            #    from lenstronomy.LightModel.Profiles.linear import LinearEllipse
+            #     self.func_list.append(Linear())
+            # elif profile_type == "LINEAR_ELLIPSE":
+            #     from lenstronomy.LightModel.Profiles.linear import LinearEllipse
 
-            #    self.func_list.append(LinearEllipse())
-            #elif profile_type == "LINE_PROFILE":
-            #    from lenstronomy.LightModel.Profiles.lineprofile import LineProfile
+            #     self.func_list.append(LinearEllipse())
+            # elif profile_type == "LINE_PROFILE":
+            #     from lenstronomy.LightModel.Profiles.lineprofile import LineProfile
 
-            #    self.func_list.append(LineProfile())
+            #     self.func_list.append(LineProfile())
             
             else:
                 raise ValueError(
@@ -242,50 +240,49 @@ class LightModelBase(object):
         return flux
 
     # TODO: Re-implement this when other profiles are added to jaxtronomy
-    """
-    def light_3d(self, r, kwargs_list, k=None):
-        Computes 3d density at radius r (3D radius) such that integrated in
-        projection in units of angle results in the projected surface brightness.
 
-        :param r: 3d radius units of arcsec relative to the center of the light profile
-        :param kwargs_list: keyword argument list of light profile
-        :param k: integer or list of integers for selecting subsets of light profiles.
-        :return: flux density
-        
-        kwargs_list_standard = self._transform_kwargs(kwargs_list)
-        r = np.array(r, dtype=float)
-        flux = np.zeros_like(r)
-        bool_list = self._bool_list(k=k)
-        for i, func in enumerate(self.func_list):
-            if bool_list[i] is True:
-                kwargs = {
-                    k: v
-                    for k, v in kwargs_list_standard[i].items()
-                    if k not in ["center_x", "center_y"]
-                }
-                if self.profile_type_list[i] in [
-                    "DOUBLE_CHAMELEON",
-                    "CHAMELEON",
-                    "HERNQUIST",
-                    "HERNQUIST_ELLIPSE",
-                    "PJAFFE",
-                    "PJAFFE_ELLIPSE",
-                    "GAUSSIAN",
-                    "GAUSSIAN_ELLIPSE",
-                    "MULTI_GAUSSIAN",
-                    "MULTI_GAUSSIAN_ELLIPSE",
-                    "NIE",
-                    "POWER_LAW",
-                    "TRIPLE_CHAMELEON",
-                ]:
-                    flux += func.light_3d(r, **kwargs)
-                else:
-                    raise ValueError(
-                        "Light model %s does not support a 3d light distribution!"
-                        % self.profile_type_list[i]
-                    )
-        return flux
-    """
+    # def light_3d(self, r, kwargs_list, k=None):
+    #     """Computes 3d density at radius r (3D radius) such that integrated in
+    #     projection in units of angle results in the projected surface brightness.
+
+    #     :param r: 3d radius units of arcsec relative to the center of the light profile
+    #     :param kwargs_list: keyword argument list of light profile
+    #     :param k: integer or list of integers for selecting subsets of light profiles.
+    #     :return: flux density
+    #     """
+    #     kwargs_list_standard = self._transform_kwargs(kwargs_list)
+    #     r = np.array(r, dtype=float)
+    #     flux = np.zeros_like(r)
+    #     bool_list = self._bool_list(k=k)
+    #     for i, func in enumerate(self.func_list):
+    #         if bool_list[i] is True:
+    #             kwargs = {
+    #                 k: v
+    #                 for k, v in kwargs_list_standard[i].items()
+    #                 if k not in ["center_x", "center_y"]
+    #             }
+    #             if self.profile_type_list[i] in [
+    #                 "DOUBLE_CHAMELEON",
+    #                 "CHAMELEON",
+    #                 "HERNQUIST",
+    #                 "HERNQUIST_ELLIPSE",
+    #                 "PJAFFE",
+    #                 "PJAFFE_ELLIPSE",
+    #                 "GAUSSIAN",
+    #                 "GAUSSIAN_ELLIPSE",
+    #                 "MULTI_GAUSSIAN",
+    #                 "MULTI_GAUSSIAN_ELLIPSE",
+    #                 "NIE",
+    #                 "POWER_LAW",
+    #                 "TRIPLE_CHAMELEON",
+    #             ]:
+    #                 flux += func.light_3d(r, **kwargs)
+    #             else:
+    #                 raise ValueError(
+    #                     "Light model %s does not support a 3d light distribution!"
+    #                     % self.profile_type_list[i]
+    #                 )
+    #     return flux
 
     def total_flux(self, kwargs_list, norm=False, k=None):
         """Computes the total flux of each individual light profile. This allows to
@@ -317,15 +314,13 @@ class LightModelBase(object):
                     kwargs_new = kwargs_list_standard[i].copy()
                     if norm is True:
                         # TODO: Re-implement when these profiles are added to jaxtronomy
-                        """
-                        if model in ["MULTI_GAUSSIAN", "MULTI_GAUSSIAN_ELLIPSE"]:
-                            new = {
-                                "amp": np.array(kwargs_new["amp"])
-                                / kwargs_new["amp"][0]
-                            }
-                        else:
-                            new = {"amp": 1}
-                        """
+                        # if model in ["MULTI_GAUSSIAN", "MULTI_GAUSSIAN_ELLIPSE"]:
+                        #     new = {
+                        #         "amp": np.array(kwargs_new["amp"])
+                        #         / kwargs_new["amp"][0]
+                        #     }
+                        # else:
+                        #     new = {"amp": 1}
                         new = {"amp": 1} # Delete this line when the above if statement is re-implemented
                         kwargs_new.update(new)
                     norm_flux = self.func_list[i].total_flux(**kwargs_new)
@@ -340,13 +335,11 @@ class LightModelBase(object):
         return norm_flux_list
 
     # TODO: Re-implement when these profiles are added to jaxtronomy
-    """
-    def delete_interpol_caches(self):
-        Call the delete_cache method of INTERPOL profiles.
-        for i, model in enumerate(self.profile_type_list):
-            if model in ["INTERPOL", "SLIT_STARLETS", "SLIT_STARLETS_GEN2"]:
-                self.func_list[i].delete_cache()
-    """
+    # def delete_interpol_caches(self):
+    #     """Call the delete_cache method of INTERPOL profiles."""
+    #     for i, model in enumerate(self.profile_type_list):
+    #         if model in ["INTERPOL", "SLIT_STARLETS", "SLIT_STARLETS_GEN2"]:
+    #             self.func_list[i].delete_cache()
 
     def _transform_kwargs(self, kwargs_list):
         """
