5459e7bde5b5d536d00336616c822680ac9c00bf
ahuang314 <alan.huang.1@stonybrook.edu>
2024-10-16 13:56:48 -0400

    Added a comment

--- jaxtronomy/ImSim/Numerics/grid.py
+++ jaxtronomy/ImSim/Numerics/grid.py
@@ -14,6 +14,8 @@ export, __all__ = exporter()
 @export
 class RegularGrid(Coordinates1D):
     """Manages a super-sampled grid on the partial image."""
+    # NOTE: JIT-compiled functions need to be recompiled each time a new instance
+    # of RegularGrid is created
 
     def __init__(
         self,
--- jaxtronomy/Util/util.py
+++ jaxtronomy/Util/util.py
@@ -1,33 +1,41 @@
 __author__ = "Simon Birrer"
 """This file contains standard routines.
 
-Copy pasted from Lenstronomy.
-Only the rotate function has been jaxified.
-TODO: Import jaxified version of everything else
-      (when applicable) and write test functions
+TODO: Import jaxified versions of other lenstronomy.Util.util.py
+      functions (when necessary)
 """
 
-from lenstronomy.Util.package_util import exporter
-import jax.numpy as jnp
-from jax import jit
+from functools import partial
+from jax import jit, numpy as jnp
+import numpy as np
+
 from lenstronomy.Util.util import make_grid, local_minima_2d, selectBest
+from lenstronomy.Util.package_util import exporter
 
 export, __all__ = exporter()
 
 
 @export
-@jit
-def rotate(xcoords, ycoords, angle):
-    """
-    :param xcoords: x points
-    :param ycoords: y points
-    :param angle: angle in radians
-    :return: x points and y points rotated ccw by angle theta
-    """
-    return xcoords * jnp.cos(angle) + ycoords * jnp.sin(angle), -xcoords * jnp.sin(
-        angle
-    ) + ycoords * jnp.cos(angle)
+@partial(jit, static_argnums=(1,2))
+def array2image(array, nx=0, ny=0):
+    """Returns the information contained in a 1d array into an n*n 2d array (only works
+    when length of array is n**2, or nx and ny are provided)
 
+    :param array: image values
+    :type array: array of size n**2
+    :returns: 2d array
+    :raises: ValueError
+    """
+    if nx == 0 or ny == 0:
+        n = int(np.sqrt(jnp.size(array)))
+        if n**2 != jnp.size(array):
+            raise ValueError(
+                "length of input array given as %s is not square of integer number!"
+                % (jnp.size(array))
+            )
+        nx, ny = n, n
+    image = jnp.reshape(array, (nx, ny))
+    return image
 
 @export
 @jit
@@ -49,3 +57,67 @@ def displaceAbs(x, y, sourcePos_x, sourcePos_y):
     y_mapped = y - sourcePos_y
     absmapped = jnp.sqrt(x_mapped**2 + y_mapped**2)
     return absmapped
+
+@export
+@jit
+def image2array(image):
+    """Returns the information contained in a 2d array into an n*n 1d array.
+
+    :param image: image values
+    :type image: array of size (n,n)
+    :returns: 1d array
+    :raises: AttributeError, KeyError
+    """
+    imgh = jnp.reshape(image, jnp.size(image))  # change the shape to be 1d
+    return imgh
+
+@export
+@partial(jit, static_argnums=2)
+def make_subgrid(ra_coord, dec_coord, subgrid_res=2):
+    """Return a grid with subgrid resolution.
+
+    :param ra_coord:
+    :param dec_coord:
+    :param subgrid_res:
+    :return:
+    """
+    ra_array = array2image(ra_coord)
+    dec_array = array2image(dec_coord)
+    n = jnp.size(ra_array, axis=0)
+    d_ra_x = ra_array.at[0,1].get() - ra_array.at[0,0].get()
+    d_ra_y = ra_array.at[1,0].get() - ra_array.at[0,0].get()
+    d_dec_x = dec_array.at[0,1].get() - dec_array.at[0,0].get()
+    d_dec_y = dec_array.at[1,0].get() - dec_array.at[0,0].get()
+
+    ra_array_new = jnp.zeros((n * subgrid_res, n * subgrid_res))
+    dec_array_new = jnp.zeros((n * subgrid_res, n * subgrid_res))
+    for i in range(0, subgrid_res):
+        for j in range(0, subgrid_res):
+            ra_array_new = ra_array_new.at[i::subgrid_res, j::subgrid_res].set(
+                ra_array
+                + d_ra_x * (-1 / 2.0 + 1 / (2.0 * subgrid_res) + j / float(subgrid_res))
+                + d_ra_y * (-1 / 2.0 + 1 / (2.0 * subgrid_res) + i / float(subgrid_res))
+            )
+            dec_array_new = dec_array_new.at[i::subgrid_res, j::subgrid_res].set(
+                dec_array
+                + d_dec_x
+                * (-1 / 2.0 + 1 / (2.0 * subgrid_res) + j / float(subgrid_res))
+                + d_dec_y
+                * (-1 / 2.0 + 1 / (2.0 * subgrid_res) + i / float(subgrid_res))
+            )
+    ra_coords_sub = image2array(ra_array_new)
+    dec_coords_sub = image2array(dec_array_new)
+    return ra_coords_sub, dec_coords_sub
+
+@export
+@jit
+def rotate(xcoords, ycoords, angle):
+    """
+    :param xcoords: x points
+    :param ycoords: y points
+    :param angle: angle in radians
+    :return: x points and y points rotated ccw by angle theta
+    """
+    return xcoords * jnp.cos(angle) + ycoords * jnp.sin(angle), -xcoords * jnp.sin(
+        angle
+    ) + ycoords * jnp.cos(angle)
