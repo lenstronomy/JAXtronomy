04158851de4529ffeb2e2e329c371622c0452897
ahuang314 <alan.huang.1@stonybrook.edu>
2025-03-08 15:24:30 -0500

    fixed incorrect argument name

--- jaxtronomy/ImSim/image_model.py
+++ jaxtronomy/ImSim/image_model.py
@@ -644,7 +644,7 @@ class ImageModel(object):
                             dec_pos,
                             None,
                             self.Data.data,
-                            fix_psf_error_map=False,
+                            fix_psf_variance_map=False,
                         )
         return error_map
 
