36b528fb2e3a7a6c9862b8ad8da99b5ce7de8373
ahuang314 <alan.huang.1@stonybrook.edu>
2025-08-04 15:38:25 -0400

    fix docstring

--- jaxtronomy/Sampling/Likelihoods/position_likelihood.py
+++ jaxtronomy/Sampling/Likelihoods/position_likelihood.py
@@ -364,7 +364,7 @@ def image2source_covariance(A, Sigma_theta):
     """Computes error covariance in the source plane.
     
     :param A: 2d array, Hessian lensing matrix
-    :param Sigma_theta: 2d array, image plane covariance matrix of uncertainties.
+    :param Sigma_theta: 2d array, image plane covariance matrix of uncertainties
     """
     ATSigma = jnp.matmul(A.T, Sigma_theta)
     return jnp.matmul(ATSigma, A)
