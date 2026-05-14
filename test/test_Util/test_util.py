26edeb065053a4f13d9d7e80fe9320953cc1c954
ahuang314 <alan.huang.1@stonybrook.edu>
2026-05-14 18:12:50 -0400

    add specific test for shift_center

--- test/test_Util/test_util.py
+++ test/test_Util/test_util.py
@@ -85,6 +85,18 @@ def test_sigma2fwhm():
     fwhm_ref = util_ref.sigma2fwhm(sigma)
     npt.assert_array_almost_equal(fwhm, fwhm_ref, decimal=8)
 
+def test_shift_center():
+    x = [1, 3, 4, 6]
+    y = [3, 40, 5, 2]
+    center_x = 3
+    center_y = 6
+
+    new_x, new_y = util.shift_center(x, y, center_x, center_y)
+    assert isinstance(new_x, jax.numpy.ndarray)
+    assert isinstance(new_y, jax.numpy.ndarray)
+
+    npt.assert_equal(np.array(new_x), [-2, 0, 1, 3])
+    npt.assert_equal(np.array(new_y), [-3, 34, -1, -4])
 
 if __name__ == "__main__":
     pytest.main()
